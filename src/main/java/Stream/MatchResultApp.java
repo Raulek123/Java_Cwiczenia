6ddbb738436b315b92eecc6b91a36d921c440d1a
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-11 18:09:01 +0200

    Add method in class main

--- src/main/java/Stream/MatchResultApp.java
+++ src/main/java/Stream/MatchResultApp.java
@@ -1,7 +1,24 @@
 package Stream;
 
+import java.util.stream.Stream;
+
 public class MatchResultApp {
     public static void main(String[] args) {
+        getMatchResultsStream();
 
+
+    }
+
+    private static Stream<MatchResult> getMatchResultsStream() {
+        return Stream.of(
+                new MatchResult("Polska", "Irlandia", 2, 0),
+                new MatchResult("Brazylia", "Francja", 0, 3),
+                new MatchResult("Włochy", "Polska", 2, 1),
+                new MatchResult("Brazylia", "Argentyna", 2, 2),
+                new MatchResult("Anglia", "Niemcy", 1, 2),
+                new MatchResult("Anglia", "Francja", 3, 0),
+                new MatchResult("Polska", "Portugalia", 1, 0),
+                new MatchResult("Brazylia", "Niemcy", 3, 3)
+        );
     }
 }
