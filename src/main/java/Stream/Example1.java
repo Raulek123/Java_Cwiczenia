95b352d6d039fb97e5abe5d2ea2fb005f98dfcdf
Raulek123 <wywial.krzysztof@wp.pl>
2023-08-11 16:44:58 +0200

    Dodanie strumienia, który spełnia poniższe założenia: Utwórz strumień kolejnych liczb całkowitych zaczynając od 0. Następnie korzystając z metody filter() wybierz tylko te z nich, które są większe od 100, mniejsze od 1000 i podzielne przez 5. Wyświetl na ekranie pierwszych 10 liczb tego strumienia pomnożonych przez 3
    Nie wykorzystuj w programie ani jednej pętli.

---
+++ src/main/java/Stream/Example1.java
@@ -0,0 +1,13 @@
+package Stream;
+
+import java.util.stream.IntStream;
+
+public class Example1 {
+    public static void main(String[] args) {
+        IntStream.iterate(0, n -> n + 1)
+                .filter(number -> number > 100 && number < 1000 && number % 5 == 0)
+                .limit(10)
+                .map(number -> number * 10)
+                .forEach(System.out::println);
+    }
+}
